94b4135cc207e947adf3747c167de8e9d04ff1f9
xamzzs <samiraraah18@gmail.com>
2025-09-04 08:18:14 +0800

    Deployment in  pythonanywhere

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3884h-q@sw)0djd0-c3_qu1qare3cma&=vtf9ijr2-!rc3cuw-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["Hesuyasu.pythonanywhere.com"]
 
 
 # Application definition
