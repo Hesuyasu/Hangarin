0231e42bd0014a0b9fb4ebb689e5d35bc5c27acc
Hesuyasu <202380031@psu.palawan.edu.ph>
2025-10-16 01:51:18 +0000

    Fixed

--- config/settings.py
+++ config/settings.py
@@ -123,9 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = (BASE_DIR / 'core' / 'static', )
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -202,3 +200,8 @@ CSRF_TRUSTED_ORIGINS = [
     "https://www.hesuyasu.pythonanywhere.com",
 ]
 
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
